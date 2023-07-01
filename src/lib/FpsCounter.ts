da23e68b29aae1b202464b3828eb9b927a7c5ec1
Benjamin Smith <bsmith@cabbage.org.uk>
2023-07-01 21:24:01 +0100

    Move FpsCounter to own file

---
+++ src/lib/FpsCounter.ts
@@ -0,0 +1,38 @@
+/* class to manage the fps counter */
+export interface FpsCounter {
+    updateFps(frameTime: number): void;
+}
+
+export interface FpsCounterConstructor {
+    new(_ : { element: null | HTMLElement }): FpsCounter;
+}
+
+export var FpsCounter = function (this: FpsCounter, { element } : { element: null | HTMLElement }) : FpsCounter {
+    let frameCount = 0;
+    let maxFrameTimeDelta = 0;
+    let countStartTime = window.performance.now();
+    let lastFrameTime = countStartTime;
+    const updateFps = element ? (frameTime: number) => {
+        frameCount++;
+
+        const frameTimeDelta = frameTime - lastFrameTime;
+        lastFrameTime = frameTime;
+        if (frameTimeDelta > maxFrameTimeDelta)
+            maxFrameTimeDelta = frameTimeDelta;
+
+        /* update every 30 frame ie approx twice per second */
+        if (frameCount === 30) {
+            const fps = (frameCount / (frameTime - countStartTime) * 1000).toPrecision(3);
+            const avg = ((frameTime - countStartTime) / frameCount).toPrecision(3);
+            const max = maxFrameTimeDelta.toPrecision(3);
+            element.innerText = `${fps} fps (avg: ${avg} ms; max: ${max} ms)`;
+
+            frameCount = 0;
+            maxFrameTimeDelta = 0;
+            countStartTime = frameTime;
+        }
+    } : () => { /* do nothing */ };
+
+    const methods = { updateFps };
+    return Object.assign(new.target ? this : Object.create(FpsCounter.prototype), methods);
+} as any as FpsCounterConstructor;
--- src/voronoi-demo.ts
+++ src/voronoi-demo.ts
@@ -1,9 +1,11 @@
 /* This is a es2016 module anyway */
 "use strict";
 
-import * as abc from 'gl-matrix';
+import { vec2 } from 'gl-matrix';
 
-const v = abc.vec2.fromValues(1, 2);
+import { FpsCounter } from './lib/FpsCounter';
+
+const v = vec2.fromValues(1, 2);
 console.log(v);
 
 /* load this file with type="module" for defer behaviour */
@@ -21,45 +23,6 @@ function printMessage(message: string) {
     // div.scrollIntoView();
     output.scrollTop = output.scrollHeight;
 }
-
-/* class to manage the fps counter */
-interface FpsCounter {
-    updateFps(frameTime: number): void;
-}
-
-interface FpsCounterConstructor {
-    new(_ : { element: null | HTMLElement }): FpsCounter;
-}
-
-var FpsCounter = function (this: FpsCounter, { element } : { element: null | HTMLElement }) : FpsCounter {
-    let frameCount = 0;
-    let maxFrameTimeDelta = 0;
-    let countStartTime = window.performance.now();
-    let lastFrameTime = countStartTime;
-    const updateFps = element ? (frameTime: number) => {
-        frameCount++;
-
-        const frameTimeDelta = frameTime - lastFrameTime;
-        lastFrameTime = frameTime;
-        if (frameTimeDelta > maxFrameTimeDelta)
-            maxFrameTimeDelta = frameTimeDelta;
-
-        /* update every 30 frame ie approx twice per second */
-        if (frameCount === 30) {
-            const fps = (frameCount / (frameTime - countStartTime) * 1000).toPrecision(3);
-            const avg = ((frameTime - countStartTime) / frameCount).toPrecision(3);
-            const max = maxFrameTimeDelta.toPrecision(3);
-            element.innerText = `${fps} fps (avg: ${avg} ms; max: ${max} ms)`;
-
-            frameCount = 0;
-            maxFrameTimeDelta = 0;
-            countStartTime = frameTime;
-        }
-    } : () => { /* do nothing */ };
-
-    const methods = { updateFps };
-    return Object.assign(new.target ? this : Object.create(FpsCounter.prototype), methods);
-} as any as FpsCounterConstructor;
 
 /* handle clicks */
 interface ClickLocation {
